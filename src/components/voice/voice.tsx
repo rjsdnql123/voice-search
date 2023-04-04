e5568bc74829e10a12a432aa918d3cc2932bd8e6
원건우 <wongeon-u@wongunwoo.local>
2023-04-05 02:31:37 +0900

    (Fix): 대화 한 내용 저장할 수 있게 수정

--- src/components/voice/voice.tsx
+++ src/components/voice/voice.tsx
@@ -1,39 +1,58 @@
-import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
-import { speechHooks } from '~/util/speech.hooks';
-import { Answer } from './answer/anwer';
-import { Question } from './question/question';
+import {
+  component$,
+  useClientEffect$,
+  useSignal,
+  useStore,
+  useTask$,
+} from "@builder.io/qwik";
+import asdf from "~/util/api";
+import { speechHooks } from "~/util/speech.hooks";
+import { Answer } from "./answer/anwer";
+import { Question } from "./question/question";
+
+interface ASD {
+  count: number;
+  voiceValue: string;
+  aiAnswer: any;
+}
 
 export const VoiceOpenAi = component$(() => {
-
   const state = useStore({
     count: 0,
-    voiceValue : ''
+    voiceValue: "",
+    aiAnswer: [],
+    dddddd: "",
+    json: [],
   });
-  
+
   useClientEffect$(() => {
     speechHooks(state);
   });
 
-  
+  useTask$(async ({ track }) => {
+    track(() => state.voiceValue);
+    // 입력이 3초 동안 안되면?
+
+    //   const timer = setTimeout(() => {
+    //     console.log('asdfdsaf')
+    //     asdf(state.voiceValue).then((res:string) => {
+    //       console.log(res)
+    //       state.aiAnswer.push(res)
+    //       state.dddddd = res
+    //     })
+    // }, 3000)
+
+    // // useEffect의 리턴부에 타이머를 해제시킨다.(클린업)
+    // return () => clearTimeout(timer)
+  });
 
   return (
+    <div>
+      <div>질문</div>
       <div>
-        <div>오픈 api를 이용해 마이크로 질문을 할 수가 있습니다.</div>
-        <div>
-        <Question question={state.voiceValue} />
-        <Answer/>
-        </div>
+        <Question question={state.voiceValue} list={state.json} />
+        <Answer aiAnser={state.aiAnswer} ddddd={state.dddddd} />
       </div>
+    </div>
   );
 });
-
-   // const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-    // const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
-    // const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-    // console.log(response)
-    
-    // const interval = setInterval(() => {
-    //   asdf(state.voiceValue)
-    // }, 2000);
-    // return () => clearInterval(interval);
